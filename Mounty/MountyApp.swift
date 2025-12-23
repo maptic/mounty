002dfdc72b10697ddbb6828fdb0594e37dddced7
Merlin Unterfinger <info@munterfinger.ch>
2025-12-23 14:45:03 +0100

    feat: add custom menu bar logo

--- Mounty/MountyApp.swift
+++ Mounty/MountyApp.swift
@@ -5,12 +5,24 @@ import SwiftUI
 struct MountyApp: App {
     @StateObject var manager = VolumeManager()
 
+    private static let paddedIcon: NSImage = {
+        guard let image = NSImage(named: "MenuIcon") else { return NSImage() }
+
+        let targetHeight: CGFloat = 15
+        let ratio = image.size.width / image.size.height
+        let targetWidth = targetHeight * ratio
+
+        image.size = NSSize(width: targetWidth, height: targetHeight)
+        image.isTemplate = true
+
+        return image
+    }()
+
     var body: some Scene {
-        MenuBarExtra(
-            "Mounty",
-            systemImage: "externaldrive.connected.to.line.below"
-        ) {
+        MenuBarExtra {
             RootView(manager: manager)
+        } label: {
+            Image(nsImage: Self.paddedIcon)
         }
         .menuBarExtraStyle(.window)
     }
